cfe2d9c9f0f989833864d7314dc93cdd23f301b6
Sean Imani <imanisean9@gmail.com>
2024-03-15 17:08:47 +0000

    stream working with mediapipe

--- src/stream.py
+++ src/stream.py
@@ -1,17 +1,59 @@
 import cv2
+import mediapipe as mp
 
-cap = cv2.VideoCapture(0)
 
-if cap.isOpened():
-    print("Camera is ready")
+FRAME_WIDTH_ID = 3
+FRAME_HEIGHT_ID = 4
+PRIMARY_CAMERA_ID = 0
 
-while cap.isOpened():
-    ret, frame = cap.read()
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+drawing_spec = mp_drawing.DrawingSpec(color=(50, 205, 50), thickness=2, circle_radius=2)
 
-    if ret:
-        cv2.imshow("frame", frame)
+def stream():
+    """
+    Stream the video from the primary camera and track the hand gestures using the MediaPipe library.
+    Get the hand landmarks and use gesture recognition model to detect the gestures.
+    """
+
+    cap = cv2.VideoCapture(PRIMARY_CAMERA_ID)
+    if cap.isOpened():
+        print("Camera is ready")
+    else:
+        print("Camera is not ready. Exiting...")
+        return
+    
+    cap.set(FRAME_WIDTH_ID, 1280)
+    cap.set(FRAME_HEIGHT_ID, 720)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            continue
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                image=frame, 
+                landmark_list=hand_landmarks, 
+                connections=mp_hands.HAND_CONNECTIONS, 
+                landmark_drawing_spec=drawing_spec, 
+                connection_drawing_spec=drawing_spec
+            )
+        
+        cv2.imshow("Hand Tracking", frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    else:
-        break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    stream()
